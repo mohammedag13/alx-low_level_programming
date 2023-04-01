6745718030460c10af223eb7e2325c6c4bf96724
mohammedag13 <mohammed.agoumil@gmail.com>
2023-03-31 19:05:15 -0700

    mozart composed his music for

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+i#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: the array to reverse
+ * @n: number of elements of the array
+ *
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = 0; i < n--; i++)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
